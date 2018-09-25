7d01ef009f42787d9e1152102be3f07d001ac894
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-09-25 22:07:37 +0300

    MPP: Fix JPS tests
    This time check both the ApplicationManager status and IDEA classes presence.
    It seems that only a single approach is not sufficient.

--- idea/idea-jps-common/src/org/jetbrains/kotlin/config/JpsUtils.kt
+++ idea/idea-jps-common/src/org/jetbrains/kotlin/config/JpsUtils.kt
@@ -5,9 +5,33 @@
 
 package org.jetbrains.kotlin.config
 
-private const val IDE_EVENT_QUEUE_CLASS_NAME = "com.intellij.ide.IdeEventQueue"
+import com.intellij.openapi.application.ApplicationManager
+
+private val IDEA_CLASSES = listOf(
+    "com.intellij.ide.IdeEventQueue",
+    "com.intellij.debugger.PositionManager"
+)
+
+private const val APPLICATION_MANAGER_CLASS_NAME = "com.intellij.openapi.application.ApplicationManager"
 
 val isJps: Boolean by lazy {
-    val ideEventQueueClassPath = IDE_EVENT_QUEUE_CLASS_NAME.replace('.', '/') + ".class"
-    {}.javaClass.classLoader.getResource(ideEventQueueClassPath) == null
+    // We run JPS tests with the whole IDEA in the classpath
+    isApplicationInitialized()?.let { return@lazy !it }
+
+    // Normally, JPS shouldn't have any of the `IDEA_CLASSES` classes in the classpath
+    return@lazy !IDEA_CLASSES.all(::doesClassExists)
+}
+
+private fun isApplicationInitialized(): Boolean? {
+    if (!doesClassExists(APPLICATION_MANAGER_CLASS_NAME)) {
+        return null
+    }
+
+    // An initialized application say we're inside the IDEA process
+    return ApplicationManager.getApplication() != null
+}
+
+private fun doesClassExists(fqName: String): Boolean {
+    val classPath = fqName.replace('.', '/') + ".class"
+    return {}.javaClass.classLoader.getResource(classPath) != null
 }
